32fc38c4648d164d30cc0fe869c62d92f939179b
Andrew Huynh <andrew.h7358@gmail.com>
2025-08-24 21:16:13 -0700

    remove useEffects and refactors

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,12 @@ interface PokemonDetails {
 }
 
 const MIN_FILTER_LENGTH = 2
+const INIT_ID = 1
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const [allPokemon, setAllPokemon] = useState([] as PokemonDetails[])
-  const [currentId, setCurrentId] = useState(1)
+  const [currentId, setCurrentId] = useState(INIT_ID)
   const [filter, setFilter] = useState('')
   const [filteredPokemon, setFilteredPokemon] = useState([] as PokemonDetails[])
   const [spriteUrl, setSpriteUrl] = useState('')
@@ -26,48 +28,42 @@ const App = () => {
       setAllPokemon(pokemon)
     }
     init()
+    onChangeId(INIT_ID)
   }, [])
 
-  useEffect(() => {
+  const onChangeId = async (id: number) => {
+    setCurrentId(id)
     setFilter('')
-  }, [currentId])
+    setFilteredPokemon([])
 
-  useEffect(() => {
-    async function getSpriteUrl() {
-      const url = await getPokemonSpriteUrl(currentId)
-      setSpriteUrl(url)
-    }
-    getSpriteUrl()
-  }, [currentId])
-
-  useEffect(() => {
-    async function getText() {
-      const text = await getPokemonText(currentId)
-      setText(`#${currentId}. ${text}`)
-    }
-    getText()
-  }, [currentId])
-
-  useEffect(() => {
-    if (filter.length >= MIN_FILTER_LENGTH) {
-      const currentIndex = currentId - 1
-      const filtered = allPokemon.filter(({ name }, index) => name.includes(filter) && index !== currentIndex)
-      const current = allPokemon[currentIndex]
-      filtered.unshift(current)
-      setFilteredPokemon(filtered)
-    } else {
-      setFilteredPokemon([])
-    }
-  }, [filter])
+    setIsLoading(true)
+    const url = await getPokemonSpriteUrl(id)
+    const text = await getPokemonText(id)
+    
+    setSpriteUrl(url)
+    setText(`#${id}. ${text}`)
+    setIsLoading(false)
+  }
 
   const onChangeFilter = (e: FormEvent) => {
     const { value } = e.target as HTMLInputElement
     setFilter(value)
+
+    if (value.length >= MIN_FILTER_LENGTH) {
+      const lowerStr = value.toLowerCase()
+      const currentIndex = currentId - 1
+      const filtered = allPokemon.filter(({ name }, index) => name.includes(lowerStr) && index !== currentIndex)
+      const current = allPokemon[currentIndex]
+      filtered.unshift(current)
+      setFilteredPokemon(filtered)
+    } else {  
+      setFilteredPokemon([])
+    }
   }
 
   const pokemon = filteredPokemon.length ? filteredPokemon : allPokemon
   const options = pokemon.map(({ id, label }) => {
-    return <option key={id} value={id} onClick={() => setCurrentId(id)}>{label}</option>
+    return <option key={id} value={id} onClick={() => onChangeId(id)}>{label}</option>
   })
 
   const sprite = <img id='sprite' className='sprite' />
@@ -77,18 +73,16 @@ const App = () => {
     spriteImgTag.src = spriteUrl
   }
 
-  const previous = <button onClick={() => setCurrentId(currentId - 1)} disabled={currentId === 1}>Previous</button>
-  const next = <button className='floatRight' onClick={() => setCurrentId(currentId + 1)} disabled={currentId === MAX_ID}>Next</button>
+  const previous = <button onClick={() => onChangeId(currentId - 1)} disabled={currentId === 1}>Previous</button>
+  const next = <button className='floatRight' onClick={() => onChangeId(currentId + 1)} disabled={currentId === MAX_ID}>Next</button>
 
   return (
     <>
       <h1>Pokédex</h1>
       <div id='card' className='card'>
         <div id='filterContainer' className='filterContainer'>
-          <label>
-            {'Filter: '}
-            <input type='text' name={'filter'} value={filter} onChange={onChangeFilter} placeholder={`min ${MIN_FILTER_LENGTH} characters`} />
-          </label>
+          <label>Filter: </label>
+          <input type='text' name={'filter'} value={filter} onChange={onChangeFilter} placeholder={`min ${MIN_FILTER_LENGTH} characters`} />
         </div>
         <select className='select' value={currentId} size={5}>
           {options}
@@ -97,7 +91,7 @@ const App = () => {
           {sprite}
         </div>
         <div id='text' className='text'>
-          {text}
+          {isLoading ? 'LOADING' : text}
         </div>
         <div id='actionsContainer' className='actionsContainer'>
           {previous}
